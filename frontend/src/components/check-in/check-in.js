57cdcacb50eb2361bab3fbe5375fb1bb93aec4eb
Nhat Trinh <goodyshoes_nt@yahoo.com>
2019-01-20 14:08:06 -0800

    finished generate code and member portal

--- frontend/src/components/check-in/check-in.js
+++ frontend/src/components/check-in/check-in.js
@@ -1,31 +1,80 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from 'axios';
 
-const CheckIn = props => (
-  <div className="container check-in">
-    <h3 className="subtitle">
-      If you attended one of our sessions either Thursday or Friday please fill
-      out form below. Thank you!
-    </h3>
-    <br />
-    <div class="field">
-      <div class="control">
-        <label>Please enter SJSU email</label>
-        <input
-          class="input is-rounded is-medium"
-          type="text"
-          placeholder="SJSU Email"
-        />
-      </div>
-      <div class="control">
-        <label>Please enter CODE provided by the Tech Lead</label>
-        <input
-          class="input is-rounded is-medium"
-          type="text"
-          placeholder="Enter Code"
-        />
-      </div>
-    </div>
-  </div>
-);
+import Button from "components/button/button";
 
-export default CheckIn;
+export default class CheckIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      code: ''
+    }
+  }
+
+  handleSubmit = () => {
+    axios.post("http://localhost:8080/api/checkin/update", { ...this.state })
+      .then(res => {
+        if (res.data.success) {
+          alert("Thanks! You are successfully checked in!");
+          window.location.replace("http://www.sjsuhoplite.org")
+        }
+      })
+      .catch(err => {
+        if (err.response.data.msg !== undefined && err.response.data.msg)
+          alert(err.response.data.msg);
+        else
+          alert('Check your form information, and try again');
+      })
+  }
+
+  handleChange = e => {
+    let { value, name } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div className="container check-in">
+        <h3 className="subtitle">
+          If you attended one of our sessions either Thursday or Friday please fill
+          out form below. Thank you!
+        </h3>
+        <br />
+        <div className="field">
+          <div className="control">
+            <label>Please enter SJSU email</label>
+            <input
+              name="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+              className="input is-rounded is-medium"
+              type="text"
+              placeholder="SJSU Email"
+            />
+          </div>
+          {/* Max length for check in code is 6 */}
+          <div className="control">
+            <label>Please enter CODE provided by the Tech Lead</label>
+            <input
+              name="code"
+              onChange={this.handleChange}
+              value={this.state.code}
+              className="input is-rounded is-medium"
+              type="text"
+              placeholder="Enter Code"
+              maxLength="7"
+            />
+          </div>
+          <div className="container is-flex actions">
+            <Button
+              className="submit-button"
+              label="Submit"
+              clicked={this.handleSubmit}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- frontend/src/components/generate-code/generate-code.js
+++ frontend/src/components/generate-code/generate-code.js
@@ -3,13 +3,16 @@ import React from "react";
 const GenerateCode = ({ code }) => (
   <div className="container check-in">
     <h3 className="subtitle">
-      This part of website is for Officer's only! Generate code for members to
+      This part of website is for officers only! Generate code for members to
       check-in.
     </h3>
-    Reresh page to generate new code. Code's expire after 5minutes.
+    Reresh page to generate new code. Codes expire after 5 minutes.
     <br />
-    <div class="control">
-      <input class="input is-large" type="text" value={code} readonly />
+    {/* <div className="control">
+      <input className="input is-large" type="text" value={code} readOnly />
+    </div> */}
+    <div className="level code-display">
+      <h2 className="level-item">{ code }</h2>
     </div>
   </div>
 );
