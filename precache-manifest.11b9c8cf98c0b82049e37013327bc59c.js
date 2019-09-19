fc1f22d4ca75673d1a73aaa9816e6925b64a1331
Jonathan Wong <chi.jonathanwong@gmail.com>
2019-09-19 09:12:34 -0700

    Updates

---
+++ precache-manifest.11b9c8cf98c0b82049e37013327bc59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d2c8bcdc8410be026a26dcbf2dd67a2",
+    "url": "/Club-Website/static/media/logo.2d2c8bcd.png"
+  },
+  {
+    "revision": "164f1a037278ae473956",
+    "url": "/Club-Website/static/js/runtime~main.8b453448.js"
+  },
+  {
+    "revision": "b98f2525ee079baedea6",
+    "url": "/Club-Website/static/js/main.c4467357.chunk.js"
+  },
+  {
+    "revision": "93aafb51e20c2af3e758",
+    "url": "/Club-Website/static/js/2.b3b7fdc7.chunk.js"
+  },
+  {
+    "revision": "b98f2525ee079baedea6",
+    "url": "/Club-Website/static/css/main.1075dc18.chunk.css"
+  },
+  {
+    "revision": "6f0ee53cc1dcec4a82ed262c3b8cb2aa",
+    "url": "/Club-Website/index.html"
+  }
+];
